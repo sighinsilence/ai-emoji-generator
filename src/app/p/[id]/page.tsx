8024fe45f2a41c62ac518993b0845f5d9240f7b5
sighinsilence <sighinsilence@gmail.com>
2025-04-09 20:28:22 +0800

    Update page.tsx

--- src/app/p/[id]/page.tsx
+++ src/app/p/[id]/page.tsx
@@ -1,19 +1,10 @@
+import { EmojiCard } from "@/app/_components/emoji-card";
+import { PageContent } from "@/app/_components/page-content";
+import { formatPrompt } from "@/lib/formatPrompt"; // 修改导入路径
+import { getEmoji } from "@/server/get-emoji";
 import { EmojiContextProps } from "@/server/utils";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
-import { formatPrompt } from "@/lib/utils";
-import { getEmoji } from "@/server/get-emoji";
-
-// 动态导入组件
-const loadEmojiCard = async () => {
-  const { EmojiCard } = await import("@/app/_components/emoji-card");
-  return EmojiCard;
-};
-
-const loadPageContent = async () => {
-  const { PageContent } = await import("@/app/_components/page-content");
-  return PageContent;
-};
 
 export async function generateMetadata({ params }: EmojiContextProps): Promise<Metadata | undefined> {
   const data = await getEmoji(params.id);
@@ -41,9 +32,6 @@ export async function generateMetadata({ params }: EmojiContextProps): Promise<M
 export default async function Emoji({ params }: EmojiContextProps) {
   const data = await getEmoji(params.id);
   if (!data) redirect("/");
-
-  const EmojiCard = await loadEmojiCard();
-  const PageContent = await loadPageContent();
 
   return (
     <PageContent prompt={data.prompt}>
